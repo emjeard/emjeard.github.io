9db5c47c76e4c3e98bacf715b61f688b803d894f
Muchamad Jeffri Ardyansyah <jevrieasay10@gmail.com>
2021-11-19 16:55:51 +0700

    basic template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Views from './views';
+import { Route, Switch } from 'react-router-dom';
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+import { THEME_CONFIG } from './configs/AppConfig';
+
+const themes = {
+  dark: `${process.env.PUBLIC_URL}/css/dark-theme.css`,
+  light: `${process.env.PUBLIC_URL}/css/light-theme.css`,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <ThemeSwitcherProvider themeMap={themes} defaultTheme={THEME_CONFIG.currentTheme} insertionPoint="styles-insertion-point">
+          <Router>
+            <Switch>
+              <Route path="/" component={Views}/>
+            </Switch>
+          </Router>
+        </ThemeSwitcherProvider>
+      </Provider>
     </div>
   );
 }
